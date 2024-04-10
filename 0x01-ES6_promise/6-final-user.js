7075ecdafb89be36afe4e278bff09c15c2192af6
ehabsmh <ehabsmh3@gmail.com>
2024-04-10 23:50:11 +0200

    6. Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,22 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // return new Promise((resolve, reject) => {
-  //   resolve(signUpUser(firstName, lastName).then((json) => [
-  //     {
-  //       status: json.status,
-  //       value: json,
-  //     },
-  //   ]));
-
-  //   reject(uploadPhoto(fileName).catch((err) => [
-  //     {
-  //       status: err.status,
-  //       value: err.message,
-  //     },
-  //   ]));
-  // });
-
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => res);
+    .then((res) => (
+      res.map((settled) => (
+        {
+          status: settled.status,
+          value: settled.status === 'fulfilled' ? settled.value : settled.reason,
+        }
+      ))
+    ));
 }
--- 0x01-ES6_promise/executables/6-main.js
+++ 0x01-ES6_promise/executables/6-main.js
@@ -1,3 +1,25 @@
 import handleProfileSignup from '../6-final-user';
 
 console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+
+/* 
+[
+    {
+      status: 'fulfilled',
+      value: { firstName: 'Bob', lastName: 'Dylan' }
+    },
+    {
+      status: 'rejected',
+      reason: Error: bob_dylan.jpg cannot be processed
+          at uploadPhoto (/home/ehab/alx/alx-backend-javascript/0x01-ES6_promise/5-photo-reject.js:2:25)
+          at handleProfileSignup (/home/ehab/alx/alx-backend-javascript/0x01-ES6_promise/6-final-user.js:7:62)
+          at Object.<anonymous> (/home/ehab/alx/alx-backend-javascript/0x01-ES6_promise/executables/6-main.js:3:32)
+          at Module._compile (internal/modules/cjs/loader.js:999:30)
+          at Module._compile (/home/ehab/alx/alx-backend-javascript/0x01-ES6_promise/node_modules/pirates/lib/index.js:117:24)
+          at Module._extensions..js (internal/modules/cjs/loader.js:1027:10)
+          at Object.newLoader [as .js] (/home/ehab/alx/alx-backend-javascript/0x01-ES6_promise/node_modules/pirates/lib/index.js:121:7)
+          at Module.load (internal/modules/cjs/loader.js:863:32)
+          at Function.Module._load (internal/modules/cjs/loader.js:708:14)
+          at Function.executeUserEntryPoint [as runMain] (internal/modules/run_main.js:60:12)
+    }
+  ] */
