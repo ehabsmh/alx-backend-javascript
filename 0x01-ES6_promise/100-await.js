4a25019cb2c383df88c4b6cd95bd664e4a8ac806
ehabsmh <ehabsmh3@gmail.com>
2024-04-11 00:40:53 +0200

    10. Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default async function asyncUploadUser() {
+  let res = {};
+  try {
+    const user = await createUser();
+    const photo = await uploadPhoto();
+    res = { photo, user };
+  } catch (_) {
+    res = { photo: null, user: null };
+  }
+
+  return res;
+}
