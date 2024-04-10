1305ea2f2da7415a0e29752c2725d33648c47af0
ehabsmh <ehabsmh3@gmail.com>
2024-04-10 22:20:21 +0200

    Error instance handled, body value corrected

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,8 @@
 // import getFullResponseFromAPI from "./1-promise";
 
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => Error(''))
+  return promise.then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error(''))
     .finally(() => {
       console.log('Got a response from the API');
     });
