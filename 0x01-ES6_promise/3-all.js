0a3dbcde2cfe857a8fd598f511fee794d91041a0
ehabsmh <ehabsmh3@gmail.com>
2024-04-10 22:56:06 +0200

    error message is corrected

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((res) => {
-      console.log(res[0].body, res[1].firstName, res[1].lastName);
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((results) => {
+      console.log(results[0].body, results[1].firstName, results[1].lastName);
     })
-    .catch(() => { console.log('Signup system offline'); });
+    .catch((error) => {
+      console.log(error.message);
+    });
 }
+handleProfileSignup();
