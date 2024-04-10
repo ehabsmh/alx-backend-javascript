570f2b2dbe89dd2776a2db1f0dfe385db972f923
ehabsmh <ehabsmh3@gmail.com>
2024-04-10 22:41:05 +0200

    3. Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const data = [];
+
+  uploadPhoto().then((json) => {
+    data.push(json.body);
+    return createUser();
+  }).then((json) => {
+    data.push(...Object.values(json));
+    console.log(data.join(' '));
+  });
+}
