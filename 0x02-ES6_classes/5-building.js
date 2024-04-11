483d5aeca543c47a4c8d24a9b71cc24b10ce32c3
ehabsmh <ehabsmh3@gmail.com>
2024-04-11 19:17:45 +0200

    5. A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (!this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== typeof (0)) throw new TypeError('sqft must be a Number');
+    this._sqft = value;
+  }
+}
