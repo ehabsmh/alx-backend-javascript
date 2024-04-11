626325b36ef9031a4c8075c93fe59972cadbbdd9
ehabsmh <ehabsmh3@gmail.com>
2024-04-11 19:33:50 +0200

    evacuationWarningMessage is defined and the condition has been set there

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,7 @@
 export default class Building {
   constructor(sqft) {
     this.sqft = sqft;
-    if (!this.evacuationWarningMessage && Object.getPrototypeOf(this) !== Building.prototype) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+    this.evacuationWarningMessage();
   }
 
   get sqft() {
@@ -13,5 +11,11 @@ export default class Building {
   set sqft(value) {
     if (typeof value !== typeof (0)) throw new TypeError('sqft must be a Number');
     this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    if (Object.getPrototypeOf(this) !== Building.prototype) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 }
