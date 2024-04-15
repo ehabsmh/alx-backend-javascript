c0eb1b26fc99d9ac7ec5862d0216a8fecdc6b586
ehabsmh <ehabsmh3@gmail.com>
2024-04-15 22:40:38 +0200

    7. More set data structure - refactored

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,15 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  let test = false;
-  const uniqueArr = [...set];
-
-  for (let i = 0; i < array.length; i += 1) {
-    for (let j = 0; j < uniqueArr.length; j += 1) {
-      if (array[i] === uniqueArr[j]) {
-        test = true;
-        break;
-      } else test = false;
-    }
-  }
-
-  return test;
+  return array.every((value) => set.has(value));
 }
