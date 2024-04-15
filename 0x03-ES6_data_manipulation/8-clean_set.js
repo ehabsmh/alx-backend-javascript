e21a23053a902481b08dc085a9ecb26df8431c76
ehabsmh <ehabsmh3@gmail.com>
2024-04-15 22:52:09 +0200

    8. Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+export default function cleanSet(set, startString) {
+  if (!startString) return '';
+  const unique = [...set];
+  const bonElements = unique.filter((ele) => ele.startsWith(startString))
+    .map((bon) => bon.slice(startString.length));
+
+  return bonElements.join('-');
+}
