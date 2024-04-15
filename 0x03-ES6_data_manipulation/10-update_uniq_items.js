8717940b78a04e91909c8961e08b96f72e248125
ehabsmh <ehabsmh3@gmail.com>
2024-04-15 23:33:29 +0200

    throw error if not Map object passed

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,6 @@
 export default function updateUniqueItems(groceriesList) {
+  if (!(groceriesList instanceof Map)) throw new Error('Cannot process');
+
   for (const [k, v] of groceriesList) {
     if (v === 1) {
       groceriesList.set(k, 100);
