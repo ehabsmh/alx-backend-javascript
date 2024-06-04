cc6e4664ba26e43e10fcda8d5f089bb025f39787
ehabsmh <ehabsmh3@gmail.com>
2024-06-04 17:38:23 +0300

    refactored

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -3,10 +3,7 @@ const fs = require('fs');
 async function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, response) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
+      if (err) reject(new Error('Cannot load the database'));
 
       const students = response
         .trim()
