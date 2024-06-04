d02e65a17417245fbf164dcef7eb36db342b77e6
ehabsmh <ehabsmh3@gmail.com>
2024-06-04 17:16:14 +0300

    logs added

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -15,20 +15,23 @@ async function countStudents(path) {
       students.shift();
       const fields = {};
 
+      const logs = [];
       students.forEach((student) => {
         if (student[3] in fields) fields[student[3]].push(student[0]);
         else fields[student[3]] = [student[0]];
       });
 
       console.log(`Number of students: ${students.length}`);
+      logs.push(`Number of students: ${students.length}`);
 
       Object.keys(fields).forEach((field) => {
         let message = `Number of students in ${field}: ${fields[field].length}.`;
         message += ` List: ${fields[field].join(', ')}`;
         console.log(message);
+        logs.push(message);
       });
 
-      resolve();
+      resolve(logs);
     });
   });
 }
