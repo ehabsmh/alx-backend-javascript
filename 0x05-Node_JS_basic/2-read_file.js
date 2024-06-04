7192af1e04cd85a197566cc8d382d1efc54143d8
ehabsmh <ehabsmh3@gmail.com>
2024-06-04 14:34:36 +0300

    buffer changed

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf-8');
+    const data = fs.readFileSync(path, 'utf8').toString();
     const studentsList = data.split('\n').slice(1);
     if (!studentsList[-1]) {
       studentsList.pop();
