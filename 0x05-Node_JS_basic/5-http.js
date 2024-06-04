96a721f899d6d133befe1645ffd3cc92cf91cd82
ehabsmh <ehabsmh3@gmail.com>
2024-06-04 17:04:54 +0300

    .

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,21 +1,22 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.method === 'GET' && req.url === '/') {
-    res.end('Hello Holberton School!');
-  }
+const PORT = 1245;
 
-  if (req.url === '/students') {
+const requestListener = async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  const { url } = req;
+  if (url === '/') res.end('Hello Holberton School!');
+  if (url === '/students') {
     const initialHeaderResponse = 'This is the list of our students\n';
     countStudents(process.argv[2])
-      .then((data) => res.end(`${initialHeaderResponse}${data.join('\n')}`))
+      .then((data) => res.end(`${initialHeaderResponse}${data}`))
       .catch((err) => res.end(`${initialHeaderResponse}${err.message}`));
   }
-});
+};
 
-app.listen(1245);
+const app = http.createServer(requestListener);
+
+app.listen(PORT);
 
 module.exports = app;
