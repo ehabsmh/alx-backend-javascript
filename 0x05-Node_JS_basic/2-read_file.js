9f6c77d02835ddaeb58e0e3ecf1014dfb025af04
ehabsmh <ehabsmh3@gmail.com>
2024-06-04 15:33:05 +0300

    refactored

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,33 +3,23 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    if (!data) {
-      console.log('Cannot load the database');
-      return;
+    const studentsList = data.split('\n').slice(1);
+    studentsList.pop();
+
+    const studentsObj = {};
+    studentsList.forEach((student) => {
+      const studentsList = student.split(',');
+      const [firstName, , , field] = studentsList;
+      if (!studentsObj[field]) studentsObj[field] = [];
+
+      studentsObj[field].push(firstName);
+    });
+
+    console.log('Number of students:', studentsList.length);
+
+    for (const student of Object.keys(studentsObj)) {
+      console.log(`Number of students in ${student}: ${studentsObj[student].length}. List: ${studentsObj[student].join(', ')}`);
     }
-    const lines = data.split('\n');
-    lines.shift(); // Remove the header line
-
-    const students = {};
-
-    lines.forEach((line) => {
-      if (line.trim() !== '') {
-        const [firstname, , , field] = line.split(',');
-        if (!students[field]) {
-          students[field] = [];
-        }
-        students[field].push(firstname);
-      }
-    });
-
-    let output = '';
-    const fields = Object.keys(students);
-    fields.forEach((field) => {
-      output += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
-    });
-
-    return output;
-
   } catch (err) {
     throw new Error('Cannot load the database');
   }
--- 0x05-Node_JS_basic/README.md
+++ 0x05-Node_JS_basic/README.md
@@ -59,3 +59,16 @@ In a file named `4-http.js`, create a small HTTP server using the `http` module:
 It should be assigned to the variable `app` and this one must be exported
 HTTP server should listen on port `1245`
 Displays `Hello Holberton School!` in the page body for any endpoint as plain text
+
+---
+
+### [5. Create a more complex HTTP server using Node's HTTP module](https://github.com/ehabsmh/alx-backend-javascript/blob/main/0x05-Node_JS_basic/5-http.js)
+
+In a file named `5-http.js`, create a small HTTP server using the `http` module:
+
+It should be assigned to the variable app and this one must be exported
+HTTP server should listen on port `1245`
+It should return plain text
+When the URL path is `/`, it should display `Hello Holberton School!` in the page body
+When the URL path is `/students`, it should display `This is the list of our students` followed by the same content as the file `3-read_file_async.js` (with and without the database) - the name of the database must be passed as argument of the file
+CSV file can contain empty lines (at the end) - and they are not a valid student!
