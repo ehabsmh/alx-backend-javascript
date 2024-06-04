0aa2037d52224cc583201f528c40c15cb041d61b
ehabsmh <ehabsmh3@gmail.com>
2024-06-04 15:19:10 +0300

    .

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,12 @@
 const http = require('node:http');
 
+const host = -'127.0.0.1';
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245, '127.0.0.1', () => { });
+app.listen(1245, host, () => { });
 
 module.exports = app;
