61935e610d7208cf72c000271f9899155a79c0ae
ehabsmh <ehabsmh3@gmail.com>
2024-06-04 17:10:03 +0300

    exported

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,7 +4,9 @@ const app = express();
 const PORT = 1245;
 
 app.get('/', (req, res) => {
-  res.end('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(PORT);
+
+module.exports = app;
