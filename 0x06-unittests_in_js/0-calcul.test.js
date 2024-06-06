3e5457fb7a6107b9b7df189eed346a7df2bee85e
ehabsmh <ehabsmh3@gmail.com>
2024-06-06 18:18:06 +0300

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,7 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const assert = require("assert");
+const { it, describe } = require("mocha");
+const calculateNumber = require("./0-calcul");
+;
 
 
 describe('calculateNumber(a, b)', () => {
