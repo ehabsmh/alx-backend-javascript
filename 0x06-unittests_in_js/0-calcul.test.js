0f1c6b40a85a7ec324770b4dbbf821ee24f1b104
ehabsmh <ehabsmh3@gmail.com>
2024-06-06 18:31:32 +0300

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,45 +1,39 @@
+/**
+ * @desc Task'0 module's test (0-calcul)
+ */
+
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-
-describe('calculateNumber(1, 3)', () => {
-    it('Should produce 4', () => {
-        assert.strictEqual(calculateNumber(1, 3), 4);
+describe('Test 0-calcul.js', function () {
+    it('a: 1.5 - b: 3.5', function () {
+        assert.equal(calculateNumber(1.5, 3.5), 6);
     });
-});
-
-describe('calculateNumber(1, 3.7)', () => {
-    it('Should produce 5', () => {
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    it('a: 1.45 - b: 3.9', function () {
+        assert.equal(calculateNumber(1.45, 3.9), 5);
     });
-});
-
-describe('calculateNumber(1.2, 3.7)', () => {
-    it('Should produce 5', () => {
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    it('a: 1.99 - b: 3.49', function () {
+        assert.equal(calculateNumber(1.99, 3.49), 5);
     });
-});
-
-describe('calculateNumber(1.5, 3.7)', () => {
-    it('Should produce 6', () => {
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    it('a: 2.7 - b: 3.9', function () {
+        assert.equal(calculateNumber(2.7, 3.9), 7);
     });
-});
-
-describe('calculateNumber(0, 0)', () => {
-    it('Should produce 0', () => {
-        assert.strictEqual(calculateNumber(0, 0), 0);
+    it('a: 11.99 - b: 12', function () {
+        assert.equal(calculateNumber(11.99, 12), 24);
     });
-});
-
-describe('calculateNumber(0.3, 0.5)', () => {
-    it('Should produce 1', () => {
-        assert.strictEqual(calculateNumber(0.3, 0.5), 1);
+    it('a: 3 - b: 4.0', function () {
+        assert.equal(calculateNumber(3, 4.0), 7);
     });
-});
-
-describe('calculateNumber(-1, -2.5)', () => {
-    it('Should produce -3', () => {
-        assert.strictEqual(calculateNumber(-1, -2.5), -3);
+    it('a: 9.489 - b: 3', function () {
+        assert.equal(calculateNumber(9.489, 3), 12);
+    });
+    it('a: 3.12 - b: 9.1', function () {
+        assert.equal(calculateNumber(3.12, 9.1), 12);
+    });
+    it('a: 0.85 - b: 3', function () {
+        assert.equal(calculateNumber(0.85, 3), 4);
+    });
+    it('a: 11 - b: -3.67', function () {
+        assert.equal(calculateNumber(11, -3.67), 7);
     });
 });
