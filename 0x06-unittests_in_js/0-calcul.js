375900cdb98caf9e2a0081f8163beddf81c71d5b
ehabsmh <ehabsmh3@gmail.com>
2024-06-06 18:14:56 +0300

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,9 +1,5 @@
 function calculateNumber(a, b) {
-    if (typeof a === Number && typeof b === Number) {
-        return a + b;
-    } else {
-        return Math.round(a) + Math.round(b);
-    }
+    return Math.round(a) + Math.round(b);
 }
 
 module.exports = calculateNumber;
