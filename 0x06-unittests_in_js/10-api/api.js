941a635d6b3825cc402a9fa194153f6c8612de0a
ehabsmh <ehabsmh3@gmail.com>
2024-06-06 22:05:58 +0300

    10. Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,30 +1,40 @@
-
 const express = require('express');
 
 const app = express();
 const port = 7865;
 
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    response.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (request, response) => {
+    response.send(`Payment methods for cart ${request.params.id}`);
+});
+
+app.get('/available_payments', (request, response) => {
+    response.set("Content-Type", "application/json");
+    const payMethods = {
+        payment_methods: {
+            credit_cards: true,
+            paypal: false
+        }
+    }
+    response.send(payMethods);
+});
+
+app.post('/login', (request, response) => {
+    const userName = request.body.userName;
+    if (userName) {
+        response.send(`Welcome ${userName}`);
+    } else {
+        response.status(404).send();
+    }
+});
+
 app.listen(port, () => {
     console.log(`API available on localhost port ${port}`);
-});
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the payment system');
-});
-
-app.get('/cart/:id(\\d+)', (req, res) => {
-    const id = req.params.id;
-
-    res.send(`Payment methods for cart ${id}`);
-});
-
-app.get('/available_payments', (_req, res) => {
-    res.json({ payment_methods: { credit_cards: true, paypal: false } });
-});
-
-app.post('/login', express.json(), (req, res) => {
-    const { userName } = req.body;
-    res.send(`Welcome ${userName}`);
 });
 
 module.exports = app;
